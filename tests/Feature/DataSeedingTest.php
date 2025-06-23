55d603202f0ef81b1695a9ad0596efb86ecbdee2
shenam19 <shenam19@gmail.com>
2025-06-23 11:27:27 +0530

    uncomment test files

--- tests/Feature/DataSeedingTest.php
+++ tests/Feature/DataSeedingTest.php
@@ -1,56 +1,57 @@
 <?php
 
-// namespace Tests\Feature;
-// use App\Models\Message;
-// use App\Models\MessageType;
-// use Illuminate\Foundation\Testing\RefreshDatabase;
-// use Illuminate\Foundation\Testing\WithFaker;
-// use Tests\TestCase;
-// use App\Models\User;
-// class DataSeedingTest extends TestCase
-// {
-//     use RefreshDatabase;
-//     use WithFaker;
+namespace Tests\Feature;
 
-//     public function setUp(): void
-//     {
-//         parent::setUp();
-//         $this->seed();
-//     }
+use App\Models\Message;
+use App\Models\MessageType;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\User;
 
-//     public function test_seeding()
-//     {
-//         $user = User::where('name', 'Front Desk of Kashag')->first(); //kashag
+class DataSeedingTest extends TestCase
+{
+  use RefreshDatabase;
+  use WithFaker;
 
-//         $UserStartId = User::where('name','Head of Kashag')->first();
-//         $UserEndId = User::where('name','Front Desk of KAS')->first();
+  public function setUp(): void
+  {
+    parent::setUp();
+    $this->seed();
+  }
 
-//         $recipient = array($UserStartId->id,$UserEndId->id);
+  public function test_seeding()
+  {
+    $user = User::where('name', 'Front Desk of Kashag')->first(); //kashag
 
-//         $MessageStartId = MessageType::where('name_tibetan','སྤྱིར་བཏང་།')->first();
-//         $MessageEndId = MessageType::where('name_tibetan','སྙན་ཞུ།')->first();
+    $UserStartId = User::where('name', 'Head of Kashag')->first();
+    $UserEndId = User::where('name', 'Front Desk of KAS')->first();
 
-//         for($i = 0 ; $i < 5; $i++)
-//         {
-//             $nameShort = strtolower(str_replace(['#', '/', '\\', ' '], '-', $user->organization->name_short));
-//             $imageUrl = $this->faker->imageUrl(640, 480, true) ?? 'path/to/default-image.png';
-//             $media = $user->addMediaFromUrl($imageUrl)
-//                          ->sanitizingFileName(function($fileName) {
-//                              return strtolower(str_replace(['#', '/', '\\', ' '], '-', $fileName));
-//                          })
-//                          ->toMediaCollection($nameShort);
-//             $outgoing_register = $user->organization->outgoingRegisterValue() + 1;
+    $recipient = array($UserStartId->id, $UserEndId->id);
 
-//             $response = $this->actingAs($user)->post('/mail/send', [
-//                 "recipients" => $this->faker->randomElement($recipient),
-//                 "message_type_id" => rand($MessageStartId->id,$MessageEndId->id),
-//                 "subject" => $this->faker->sentence(),
-//                 "record" => ["letter_number" => "kashag","outgoing_no" => $outgoing_register],
-//                 "media" => $media->id,
-//                 "remarks" => $this->faker->sentence(),
-//                 "status" => "sent",
-//             ]);
-//         }
-//         $this->assertEquals(5, Message::all()->count());
-//     }
-// }
+    $MessageStartId = MessageType::where('name_tibetan', 'སྤྱིར་བཏང་།')->first();
+    $MessageEndId = MessageType::where('name_tibetan', 'སྙན་ཞུ།')->first();
+
+    for ($i = 0; $i < 5; $i++) {
+      $nameShort = strtolower(str_replace(['#', '/', '\\', ' '], '-', $user->organization->name_short));
+      $imageUrl = $this->faker->imageUrl(640, 480, true) ?? 'path/to/default-image.png';
+      $media = $user->addMediaFromUrl($imageUrl)
+        ->sanitizingFileName(function ($fileName) {
+          return strtolower(str_replace(['#', '/', '\\', ' '], '-', $fileName));
+        })
+        ->toMediaCollection($nameShort);
+      $outgoing_register = $user->organization->outgoingRegisterValue() + 1;
+
+      $response = $this->actingAs($user)->post('/mail/send', [
+        "recipients" => $this->faker->randomElement($recipient),
+        "message_type_id" => rand($MessageStartId->id, $MessageEndId->id),
+        "subject" => $this->faker->sentence(),
+        "record" => ["letter_number" => "kashag", "outgoing_no" => $outgoing_register],
+        "media" => $media->id,
+        "remarks" => $this->faker->sentence(),
+        "status" => "sent",
+      ]);
+    }
+    $this->assertEquals(5, Message::all()->count());
+  }
+}
